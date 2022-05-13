f834bb89db50791efd9d85516ae7356b5e1397da
Luc Fujitsu <luc1024@gmail.com>
2022-05-13 18:36:05 +0300

    Merge branch 'refactor_lista_itemi'
    # Conflicts:
    #	lista_itemi.py

--- lista_itemi.py
+++ lista_itemi.py
@@ -1,33 +1,28 @@
-import os
 import docx  # "pip install python-docx==0.8.10": https://automatetheboringstuff.com/2e/chapter15/
 from docx.document import Document  # https://stackoverflow.com/a/52466366/2358837
 
 from prepare_items import get_ord_spreadsheet_values
+from env_class import Env
 
-from dotenv import load_dotenv
 
-load_dotenv()
+env = Env()
 
 
 def write_liste_itemi(sheet_id, json_keyfile_name, output_folder):
     ord_materii_values = get_ord_spreadsheet_values(sheet_id, json_keyfile_name)
 
-    OFFSET_COL = 4  # timestamp, email, categoria, materia
-    NR_MATERII = len(ord_materii_values)
-    NR_VARIANTE_RASPUNS = 4
-    NR_PARAGRAPHS_PER_ITEM = NR_VARIANTE_RASPUNS + 1  # intrebarea + variantele de raspuns
-    NR_ITEMI = []
+    nr_itemi = []
     for row in ord_materii_values:
         row = list(filter(None, row))  # elimina ''
-        NR_ITEMI.append((len(row) - OFFSET_COL) // NR_PARAGRAPHS_PER_ITEM)
+        nr_itemi.append((len(row) - env.offset_col) // env.nr_paragraphs_per_item)
     num = ['a) ', 'b) ', 'c) ', 'd) ']
-    for i in range(NR_MATERII):
+    for i in range(env.nr_materii):
         doc: docx.document.Document = docx.Document(
             'templates/Template.docx')  # https://stackoverflow.com/a/61822452/2358837
-        for j in range(NR_ITEMI[i]):
-            for k in range(NR_PARAGRAPHS_PER_ITEM):
+        for j in range(nr_itemi[i]):
+            for k in range(env.nr_paragraphs_per_item):
                 pre = str(j + 1) + '. ' if k == 0 else num[k - 1]
-                text = pre + ord_materii_values[i][OFFSET_COL + j * NR_PARAGRAPHS_PER_ITEM + k].strip()
+                text = pre + ord_materii_values[i][env.offset_col + j * env.nr_paragraphs_per_item + k].strip()
                 if j == 0 and k == 0:
                     doc.paragraphs[0].text = text
                 else:
@@ -40,7 +35,7 @@ def write_liste_itemi(sheet_id, json_keyfile_name, output_folder):
             email = ord_materii_values[i][1]
             file_name = ('%02d.' % int(nr_materie)) + materia \
                         + ' [' + email + ']' \
-                        + ' (' + str(NR_ITEMI[i]) + ' itemi)'
+                        + ' (' + str(nr_itemi[i]) + ' itemi)'
             doc.save(output_folder + 'Liste_Itemi/' + file_name + '.docx')
-    print('nr_materii: ', NR_MATERII)
+    print('env.nr_materii: ', env.nr_materii)
 
